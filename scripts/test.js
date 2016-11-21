3bc681f9877015a7ce28a89ab329ad9eb17621d3
Florian Maunier <fmaunier@gmail.com>
2016-11-21 23:20:29 +0100

    Improve test configuration

--- scripts/test.js
+++ scripts/test.js
@@ -1,18 +1,10 @@
-var debug = require('debug')('scripts:test');
-var Jasmine = require('jasmine');
-var JasmineConsoleReporter = require('jasmine-console-reporter');
-var JasmineReporters = require('jasmine-reporters');
+import Jasmine from "jasmine";
+import {JUnitXmlReporter, TapReporter} from "jasmine-reporters";
 
 var jasmine = new Jasmine();
 jasmine.loadConfigFile('test/support/jasmine.json');
 
-jasmine.addReporter(new JasmineConsoleReporter({
-	  colors: 1,
-	  cleanStack: 1,
-	  verbosity: 4,
-	  listStyle: 'indent',
-	  activity: false
-}));
-jasmine.addReporter(new JasmineReporters.JUnitXmlReporter());
+jasmine.addReporter(new TapReporter());
+jasmine.addReporter(new JUnitXmlReporter());
 
 jasmine.execute();
