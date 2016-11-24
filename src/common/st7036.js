8ae46f0e33905c311cd8f4fccfea339046546027
Florian Maunier <fmaunier@gmail.com>
2016-11-24 15:33:19 +0100

    chore: tidy up stuff

--- src/common/st7036.js
+++ src/common/st7036.js
@@ -1,19 +1,8 @@
-import rpio from "rpio";
-import SPI from "spi-device";
-import dbg from "debug";
+import rpio from 'rpio';
+import spiDevice from 'spi-device';
+import dbg from 'debug';
 
-const debug = dbg('dot:st7036');
-
-export const COMMAND_CLEAR = 0b00000001
-export const COMMAND_HOME = 0b00000010
-export const COMMAND_SCROLL = 0b00010000
-export const COMMAND_DOUBLE = 0b00010000
-export const COMMAND_BIAS = 0b00010100
-export const COMMAND_SET_DISPLAY_MODE = 0b00001000
-
-export const BLINK_ON = 0b00000001
-export const CURSOR_ON = 0b00000010
-export const DISPLAY_ON = 0b00000100
+const debug = dbg('dot:common:st7036');
 
 export class ST7036 {
   constructor(registerSelectPin, resetPin = null, rows = 3, columns = 16, spiChipSelect = 0, instructionSetTemplate = 0b00111000) {
@@ -33,7 +22,7 @@ export class ST7036 {
     this.doubleHeight = 0;
     this.animations = new Array(8).fill(null);
 
-    this.spi = SPI.openSync(0, spiChipSelect, {maxSpeedHz: 1000000});
+    this.spi = spiDevice.openSync(0, spiChipSelect, {maxSpeedHz: 1000000});
 
     if (this.resetPin !== null) {
       rpio.open(this.resetPin, rpio.OUTPUT, rpio.LOW);
@@ -51,16 +40,16 @@ export class ST7036 {
   }
 
   setBias(bias = 1) {
-    this._writeCommand(COMMAND_BIAS | (bias << 4) | 1, 1);
+    this._writeCommand(0b00010100 | (bias << 4) | 1, 1);
   }
 
   setContrast(contrast) {
-    if (typeof(contrast) !== "number") {
-      throw new TypeError("contrast must be a number");
+    if (typeof(contrast) !== 'number') {
+      throw new TypeError('contrast must be a number');
     }
 
     if (contrast < 0 || contrast > 0x3F) {
-      throw new RangeError("contrast must be an integer in the range 0..0x3F");
+      throw new RangeError('contrast must be an integer in the range 0..0x3F');
     }
 
     this._writeCommand((0b01010100 | ((contrast >> 4) & 0x03)), 1);
@@ -76,16 +65,10 @@ export class ST7036 {
   }
 
   updateDisplayMode() {
-    let command = COMMAND_SET_DISPLAY_MODE;
-    command |= this.enabled
-      ? DISPLAY_ON
-      : 0;
-    command |= this.cursorEnabled
-      ? CURSOR_ON
-      : 0;
-    command |= this.cursorBlink
-      ? BLINK_ON
-      : 0;
+    let command = 0b00001000;
+    command |= this.enabled ? 0b00000100 : 0;
+    command |= this.cursorEnabled ? 0b00000010 : 0;
+    command |= this.cursorBlink ? 0b00000001 : 0;
     this._writeCommand(command);
   }
 
@@ -105,7 +88,7 @@ export class ST7036 {
 
   setCursorPosition(column, row) {
     if (column < 0 || column > this.columns || row < 0 || row > this.rows) {
-      throw new RangeError("row and column must integers within the defined screen size");
+      throw new RangeError('row and column must integers within the defined screen size');
     }
 
     let offset = this.rowOffsets[row] + column;
@@ -118,7 +101,7 @@ export class ST7036 {
   }
 
   clear() {
-    this._writeCommand(COMMAND_CLEAR);
+    this._writeCommand(0b00000001);
     rpio.usleep(1.5)
     this.home();
   }
@@ -127,29 +110,31 @@ export class ST7036 {
     rpio.write(this.registerSelectPin, rpio.HIGH);
     [...value].forEach((chr) => {
       let buf = Buffer.from([chr.charCodeAt()]);
-      this.spi.transferSync([{
-        byteLength: buf.length,
-        sendBuffer: buf
-      }]);
+      this.spi.transferSync([
+        {
+          byteLength: buf.length,
+          sendBuffer: buf
+        }
+      ]);
       rpio.usleep(0.05);
     });
   }
 
   createAnimation(animPos, animMap, frameRate) {
     if (animPos < 0 || animPos >= this.animations.length) {
-      throw new RangeError("Valid animation positions are 0 to " + (this.animations.length - 1).toString());
+      throw new RangeError('Valid animation positions are 0 to ' + (this.animations.length - 1).toString());
     }
 
     if (!Array.isArray(animMap)) {
-      throw new TypeError("Animation map should be a list of animation frames");
+      throw new TypeError('Animation map should be a list of animation frames');
     }
 
     if (!Array.isArray(animMap[0])) {
-      throw new TypeError("Animation frames should be lists of 8 bytes");
+      throw new TypeError('Animation frames should be lists of 8 bytes');
     }
 
     if (animMap[0].length < 8) {
-      throw new RangeError("Animation frames should be lists of 8 bytes");
+      throw new RangeError('Animation frames should be lists of 8 bytes');
     }
 
     this.createChar(animPos, animMap[0]);
@@ -175,7 +160,7 @@ export class ST7036 {
     }
 
     let baseAddress = charPos * 8;
-    for(let i = 0; i < 8; i++) {
+    for (let i = 0; i < 8; i++) {
       this._writeCommand(0x40 | (baseAddress + i));
       this._writeChar(charMap[i]);
     }
@@ -184,44 +169,48 @@ export class ST7036 {
   }
 
   cursorLeft() {
-    this._writeCommand(COMMAND_SCROLL);
+    this._writeCommand(0b00010000);
   }
 
   cursorRight() {
-    this._writeCommand(COMMAND_SCROLL | (1 << 2));
+    this._writeCommand(0b00010000 | (1 << 2));
   }
 
   shiftLeft() {
-    this._writeCommand(COMMAND_SCROLL | (1 << 3));
+    this._writeCommand(0b00010000 | (1 << 3));
   }
 
   shiftRight() {
-    this._writeCommand(COMMAND_SCROLL | (1 << 3) | (1 << 2));
+    this._writeCommand(0b00010000 | (1 << 3) | (1 << 2));
   }
 
   doubleHeight(enable = 0, position = 1) {
     this.doubleHeight = enable;
     this._writeInstructionSet(0);
-    this._writeCommand(COMMAND_DOUBLE | (position << 3), 2);
+    this._writeCommand(0b00010000 | (position << 3), 2);
   }
 
   _writeChar(value) {
     rpio.write(this.registerSelectPin, rpio.HIGH);
     let buf = Buffer.from([value]);
-    this.spi.transferSync([{
-      byteLength: buf.length,
-      sendBuffer: buf
-    }]);
+    this.spi.transferSync([
+      {
+        byteLength: buf.length,
+        sendBuffer: buf
+      }
+    ]);
     rpio.usleep(0.1);
   }
 
   _writeInstructionSet(instructionSet = 0) {
     rpio.write(this.registerSelectPin, rpio.LOW);
     let buf = Buffer.from([this.instructionSetTemplate | instructionSet | (this.doubleHeight << 2)]);
-    this.spi.transferSync([{
-      byteLength: buf.length,
-      sendBuffer: buf
-    }]);
+    this.spi.transferSync([
+      {
+        byteLength: buf.length,
+        sendBuffer: buf
+      }
+    ]);
     rpio.usleep(0.06);
   }
 
@@ -229,10 +218,12 @@ export class ST7036 {
     rpio.write(this.registerSelectPin, rpio.LOW);
     this._writeInstructionSet(instructionSet);
     let buf = Buffer.from([value]);
-    this.spi.transferSync([{
-      byteLength: buf.length,
-      sendBuffer: buf
-    }]);
+    this.spi.transferSync([
+      {
+        byteLength: buf.length,
+        sendBuffer: buf
+      }
+    ]);
     rpio.usleep(0.06);
   }
 }
